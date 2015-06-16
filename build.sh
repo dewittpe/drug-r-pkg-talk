b113a265428aba4ae5a76c9b5e4c07eaca0c3e54
Peter DeWitt (UCD) <peter.dewitt@ucdenver.edu>
2015-06-16 11:47:24 -0600

    initial draft of the slides done

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-R -e "knitr::knit(input = 'simple-r-packages.Rnw', output = 'simple-r-packages.tex')"
+R --vanilla -e "knitr::knit(input = 'simple-r-packages.Rnw', output = 'simple-r-packages.R', tangle = TRUE)" > R.log
+R --vanilla -e "knitr::knit(input = 'simple-r-packages.Rnw', output = 'simple-r-packages.tex')" >> R.log
 
 pdflatex simple-r-packages.tex
 pdflatex simple-r-packages.tex
